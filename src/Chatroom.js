a3665d32edbe2a3c75223cd0a18079a655e18e9a
Madison Hillyard <madison.hillyard@libertymutual.com>
2018-07-10 10:27:29 -0400

    Welcome message added

--- src/Chatroom.js
+++ src/Chatroom.js
@@ -5,13 +5,7 @@ import './App.css';
 import Message from './Message.js';
 import Logo from './logo.svg';
 
-var Welcome = {
-    username: "LadyLiberty",
-    content: <p>Hello I'm LadyLiberty, here to help you with all your 
-                Liberty Mutual Insurance Questions. What can I help you
-                with today?</p>,
-    img: Logo,
-};
+
 class Chatroom extends React.Component {
     
     constructor(props) {
