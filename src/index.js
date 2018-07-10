fbb59f132861deb21584316122d445003c2af488
Samantha Ciaramitaro <Samantha.Ciaramitaro@libertymutual.com>
2018-07-10 10:16:57 -0400

    Connected to firebase api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from "firebase";
+
+// Initialize Firebase
+let config = {
+    apiKey: "AIzaSyA_Fawyu29Zji3PYY6KSCgtqyf1rvI2Obw",
+    authDomain: "ladyliberty-89a90.firebaseapp.com",
+    databaseURL: "https://ladyliberty-89a90.firebaseio.com",
+    storageBucket: "ladyliberty-89a90.appspot.com",
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
